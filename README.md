ab9fb735a81529d3cd99755464f8b8ceabbc145b
Ryan Flanagan <flanagan.ryan@gmail.com>
2020-09-03 17:45:31 -0400

    re-structured my script.js file and removed sandbox.html and test.html. Improved formatting of conditional statements and fixed my for Loop.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Js-Password-Generator
 To create an application that generates a random password based on user-selected criteria.
+
+## Contributions
+<!-- 1. Youtube: https://youtu.be/9sT03jEwcaw - for setting up values and for loop. -->
+<!-- https://stackoverflow.com/questions/40409749/random-string-generator-javascript-while-loop to  -->
+<!-- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/charAt - to pull a random character in a for loop -->
+<!--https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript>
+<!--https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/isNaN>
+<!--https://developer.mozilla.org/en-US/docs/Glossary/Falsy>
+
+<!--On 9-3-2020, I worked with Christina Starr and my tutor Ariel Razonwho helped me understand a few specific points to include: NaN 'Not a Number' property for line 53 and falsy values '!var' for line 91.>
+
+<!--Ariel Razon also helped me format my for loop to take in my passwordLength so the return value stops at the desired password length. Then worked with me to create a function that randomizes and joins together the elements within the users' selected arrays - and returns as a string.
--- script.js
+++ script.js
@@ -18,8 +18,6 @@
 // THEN the password is either displayed in an alert or written to the page
 // ```
 
-
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -29,74 +27,79 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-function generatePassword(){
-    //TODO: Generate a Real Password and Replace the Return String with a Real Password String
-    return "THIS IS NOT MY FINAL PASSWORD!" //needs to be executed as a string!
+//I need to create arrays that will hold all possible elements available to be used should a user select that option for their password.
+var lower = ["a", "b", "c", "d", "e","f", "g", "h", "i", "j","k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upper = ["A", "B", "C", "D", "E","F","G","H","I", "J", "K", "L", "M", "N", "O", "P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "{", "}",];
 
+//I need to create a new array that will be used to take in the chosen array groups of the user.
+var mergedValues = [];
 
+function generatePassword()
+{
 
+//Test the console
+console.log("Congratulations! I'm working.");
 
+//WHEN prompted for the length of the password
+//THEN I choose a length of at least 8 characters and no more than 128 characters
+var passwordLength = +(prompt("Choose a desired number of password characters between 8 and 128."));
 
+//If user provides a valid number that meets all the criteria below alert them that we have recieved it and log it to the console.
+//Here I show that passwordLength must be greater or equal to 8 AND less than or equal to 128 AND passwordLength is not empty AND passwordLength is not 'Not a Number'.
+    if (passwordLength >= 8 && passwordLength <= 128 && passwordLength !== null && passwordLength !== NaN) 
+     {
+    alert("Thanks. You have chosen " + passwordLength);
+    console.log(passwordLength);
+     } 
+    else {
+    alert("Your response must be a number between 8 and 128!");
+    return; //return ends this function.
+    };
 
-    //All MY CODE GOES HERE
+//WHEN prompted for character types to include in the password
+//THEN I choose lowercase, uppercase, numeric, and/or special characters
+//I need to create confirmation variables for each of the confirms.
+var confirmLower = confirm("Do you want to include lowercase letters?"); 
+var confirmUpper = confirm("Do you want to include uppercase letters?"); 
+var confirmNum = confirm("Do you want to include numbers?");
+var confirmSpecial = confirm("Do you want to include special characters?"); 
 
+//Only need to see if the user confirms for a desired option: lowercase, uppercase, number or special character - so then I don't need else statements.
+//The functions inside each 'if' statement bring in the respective array only 'if' the user has selected that specific option for their password through the confirm prompts.
+//Otherwise it moves to the next variable and either enters that into the mergedValues array or not.
+    if (confirmLower) {
+        mergedValues = mergedValues.concat(lower);
+        console.log(mergedValues);
+    }
+    if (confirmUpper) {
+        mergedValues = mergedValues.concat(upper);
+            console.log(mergedValues);
+    }
+    if (confirmNum) {
+        mergedValues = mergedValues.concat(numbers);
+                console.log(mergedValues);
+    }
+    if (confirmSpecial); {
+        mergedValues = mergedValues.concat(specialCharacters);
+                    console.log(mergedValues);
+    };
 
-    // 1. Arrays I'll Need
-    // var password = [] * needs to aggregate selections from whether or not the user wants to include any of the various arrays below
+//I need to ensure that at least one of the preceding selections was met so that a password can be produced. 
+    if (!confirmSpecial && !confirmNum && !confirmUpper && !confirmLower){
+            alert("You must select a type of character to use in your password")
+            return;
+    }
+var passwordGenerated = [];
 
-    //2. Various Arrays
-    // var characterCount = []
-    // var assignedLowercase = []
-    // var assignedUppercase = []
-    // var assignedNum = []
-    // var assignedSpecial = []
-    // var lowercase = ["a", "b", "c", "d", "e", "f", "g","h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-    // var uppercase = ["A", "B", "C", "D", "E", "F","G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-    // var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    // var specialCharacters = ["!", "#", "$", "%", "*", "&", "+", "@", "?", "~"]
-
-    //3. Prompts and Confirms 
-        // Part 1: Character Length
-        // var characterCount = prompt(Choose a desired length between 8 and 128 characters for your new password.)
-            // -->console.log the user's inputted value [To test]
-            // -->provide an alert to show the user that we've logged their entry
-
-        //Part 2: Record Password Preferences usings Confirms and Conditional Statements
-            //a. First confirm whether or not the user wants to include lowercase characters in their password.
-            
-                // If they do then I need to create a For loop for var lowercase and assign a random letter as a string to var assignedLowercase.
-
-                // EXAMPLE
-                // for (var = 0; i<lowercase.length; i++) {
-                //       var assignedLowercase = Math.floor(Math.random() * lowercase.length)];
-                //    console.log(assignedLowercase)
-                //    } --> This loops the items in the lowercase array and assigns input as chosen
-
-            //b.  If they do not then I want to progress to the next confirm question: "Do you want to include uppercase letters?"
-
-                // If they want uppercase letters then I need to create a For loop for var uppercase and assign a random letter as a string to var assignedLowercase.
-
-                // EXAMPLE
-                // for (var = 0; i<lowercase.length; i++) {
-                //       var assignedLowercase = Math.floor(Math.random() * lowercase.length)];
-                //    console.log(assignedLowercase)
-                //    } --> This loops the items in the lowercase array and assigns input as chosen
-
-            // Tip: I also need to concatenate random lowercase letter with random lowercase letter if both confirms have been selected
-
-
-
-
-
-
-
-            // At the end I need to do a For loop that takes in the total characterCount they inputted and return a random combination of their confirm selections as a String.
-
-
-
+//Now I need to run a for loop that starts at 0 and continues to run only until it hits the passwordLength variable I created earlier.
+    for (i=0;i<passwordLength;i++){
+        passwordGenerated.push(mergedValues[Math.floor(Math.random() * mergedValues.length)])
+    }
+return passwordGenerated.join("");
 
 }
 
